37c36c95fd5071c216c4d04081d08c127d5d23b0
georges ryssen <georges.ryssen@gmail.com>
2022-11-16 18:18:07 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+puts 'start seed'
+10.times do
+  Flat.create!(
+    name: Faker::Commerce.vendor,
+    address: Faker::Address.full_address,
+    description: Faker::Commerce.department(max: 5),
+    price_per_night: (50..200).to_a.sample,
+    number_of_guests: (2..12).to_a.sample
+  )
+  puts 'new flat'
+end
+puts 'finish seed'
